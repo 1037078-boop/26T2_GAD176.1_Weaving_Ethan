5a5045318f4bfe24c13ad9ccb295193b80834835
Ethan <1037078@student.sae.edu.au>
2026-07-30 10:28:22 +1000

    Maintained scripts
    added comments and changed where variables are

--- Assets/Quests/scripts/Explore.cs
+++ Assets/Quests/scripts/Explore.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+//Attention coder if you are reading this you are trying to change or add to this script.
+//I would like to let you know that when I wrote this code only god and I knew it
+//Now only God knows how it works.
+//Good luck
 public class Explore : Quests
 {
-
+    //The quest that focuses on the player exploring
 
     void Start()
     {
-        questActive = false;
+        questActive = true;
         Name = "Explore";
 
         Debug.Log("Quest: " + Name);
@@ -15,23 +18,21 @@ public class Explore : Quests
     }
     public override void Reward()
     {
-        base.Reward();
-        Debug.Log("You feel rewared");
+        Debug.Log("You feel rewarded");
     }
 
     void Update()
     {
-        if (Keyboard.current.eKey.wasPressedThisFrame)
-            { 
-            questActive = true;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (questActive == true)
+            {
+                Debug.Log("Explore the world!");
+                Reward();
+                questActive = false;
+                EndQuest();
             }
-        if (questActive == true)
-               {
-            Debug.Log("Explore the world!");
-            Reward();
-            questActive = false;
-               }
-
+        }
 
     }
 }
